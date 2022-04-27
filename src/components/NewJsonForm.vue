c80bf51698d4cd330550a69caa2d426245097b1c
639852 <sasha.bobyliev@mail.ru>
2022-04-27 16:41:15 +0400

    Create project

---
+++ src/components/NewJsonForm.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-form v-model="isValid" v-if="toggleNewFields" ref="form">
+    <v-text-field
+      required
+      label="Key"
+      v-model="newKey"
+      v-if="mode ==='add'"
+      :rules="generalRule"
+    />
+    <v-select
+      required
+      label="Choose type for value"
+      v-model="typeValue"
+      :items="typesValue"
+      :rules="generalRule"
+    />
+    <v-text-field
+      label="Value"
+      v-model="newValue"
+      :rules="typeRule"
+    />
+    <v-btn
+      dark
+      color="success"
+      :disabled="!isValid"
+      @click="changeField"
+    >
+      Save
+    </v-btn>
+  </v-form>
+</template>
+
+<script>
+export default {
+  props: {
+    toggleNewFields: { type: Boolean, required: true },
+    generalRule: { type: Array, required: true },
+    fieldKey: { type: String, required: true },
+    mode: { type: String, required: true }
+  },
+  data: () => ({
+    isValid: false,
+    newKey: '',
+    newValue: '',
+    typeValue: '',
+    typesValue: ['string', 'number', 'boolean', 'object']
+  }),
+  methods: {
+    changeField () {
+      const value = (this.typeValue === 'string')
+        ? this.newValue
+        : JSON.parse(this.newValue)
+      let tree = JSON.parse(localStorage.getItem('JSON'))
+
+      if (tree) this.findNode(tree, value)
+      else tree = { [this.newKey]: value }
+
+      localStorage.setItem('JSON', JSON.stringify(tree))
+      this.$refs.form.reset()
+      this.$emit('changeToggle')
+    },
+    findNode (tree, newValue) {
+      for (const [key, value] of Object.entries(tree)) {
+        switch (this.fieldKey) {
+          case 'New key':
+            tree[this.newKey] = newValue
+            return
+          case key:
+            if (this.mode === 'add') {
+              tree[key][this.newKey] = newValue
+            } else {
+              tree[this.fieldKey] = newValue
+            }
+            return
+          default:
+            this.findNode(value, newValue)
+        }
+      }
+    }
+  },
+  computed: {
+    typeRule () {
+      return [v => {
+        try {
+          const value = (this.typeValue !== 'string') ? JSON.parse(v) : v
+
+          switch (this.typeValue) {
+            case typeof value:
+              return !!v
+            default:
+              throw TypeError('Invalid value type!')
+          }
+        } catch (e) {
+          return e.message
+        }
+      }]
+    }
+  }
+}
+</script>
---
+++ src/components/NewJsonTextarea.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-col>
+    <v-form v-model="isValid">
+      <v-textarea
+        v-model="tree"
+        :disabled="toggleNewFields"
+        :rules="typeRule"
+      ></v-textarea>
+      <v-btn
+        dark
+        color="success"
+        :disabled="!isValid || toggleNewFields"
+        @click="changeField"
+      >
+        Save
+      </v-btn>
+    </v-form>
+  </v-col>
+</template>
+
+<script>
+export default {
+  props: {
+    toggleNewFields: { type: Boolean, required: true }
+  },
+  data: () => ({
+    tree: '',
+    isValid: true
+  }),
+  methods: {
+    changeTree () {
+      if (!localStorage.getItem('JSON')) return
+
+      const brackets = []
+
+      this.tree = localStorage.getItem('JSON')
+        .split('')
+        .map((letter) => {
+          switch (letter) {
+            case '{':
+              brackets.push('{')
+              return `{\n${'\t'.repeat(brackets.length)}`
+            case '}':
+              brackets.pop()
+              return `\n${'\t'.repeat(brackets.length)}}`
+            case ':':
+              return ': '
+            case ',':
+              return `,\n${'\t'.repeat(brackets.length)}`
+            default:
+              return letter
+          }
+        })
+        .join('')
+    },
+    changeField () {
+      const tree = JSON.parse(this.tree)
+
+      localStorage.setItem('JSON', JSON.stringify(tree))
+      this.isValid = false
+    }
+  },
+  mounted () {
+    this.changeTree()
+  },
+  watch: {
+    toggleNewFields () {
+      this.changeTree()
+    }
+  },
+  computed: {
+    typeRule () {
+      return [v => {
+        try {
+          return !!JSON.parse(v)
+        } catch (e) {
+          return e.message
+        }
+      }]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
